4291c4db8756c3c6d7a1310c649033dd57cd34c6
Phil Simpson <phil_simpson@att.net>
2023-03-20 19:29:22 -0500

    creation form start

--- src/components/CreationForm.js
+++ src/components/CreationForm.js
@@ -1,10 +1,30 @@
 import { useState } from 'react';
 //import { Modal, ModalHeader, ModalBody, FormGroup } from 'reactstrap';
-import { Modal, TextField, Button, } from '@mui/material';
+import { Modal, TextField, Button, Box, Container} from '@mui/material';
 import { Formik, Form, ErrorMessage } from 'formik';
 
 const CreationForm = () => {
-    return;
+    return (
+        <>
+            <Formik>
+                <Box>
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                </Box>
+            </Formik>
+        </>
+    );
 }
 
 export default CreationForm;
