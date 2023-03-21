6de27c452c25c43aceb3a67fb912dd692eb7e16a
Phil Simpson <phil_simpson@att.net>
2023-03-21 18:45:43 -0500

    form issues

--- src/components/CreationForm.js
+++ src/components/CreationForm.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 //import { Modal, ModalHeader, ModalBody, FormGroup } from 'reactstrap';
-import { Modal, TextField, Button, Box, Container, MenuItem} from '@mui/material';
+import { Modal, TextField, Button, Box, Container, MenuItem, FormControl} from '@mui/material';
 import { Formik, Form, ErrorMessage } from 'formik';
 import { LEVELS, RACES, CLASSES, GENDER } from '../app/shared/CHARACTEROPTIONS';
+import { positions } from '@mui/system';
 
 const CreationForm = () => {
     const charLevels = LEVELS;
@@ -18,65 +19,79 @@ const CreationForm = () => {
                     noValidate
                     autoComplete='off'
                 >
-                    <TextField 
+                        <TextField 
+                            required
+                            id='outlined-required'
+                            label='Character Name'
+                            sx={{ m: 1, width: '50ch' }}
+                        />
+                        <TextField 
+                            required
+                            id='outlined-select-required'
+                            select
+                            label='Level'
+                            defaultValue='1'
+                            helperText='Starting Level'
+                            sx={{ m: 1, width: '10ch' }}
+                        >
+                            {charLevels.map((level) => (
+                                <MenuItem key={level.value} value={level.value}>
+                                    {level.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField 
+                            required
+                            id='outlined-select-required'
+                            select
+                            label='Race'
+                            defaultValue='Select'
+                            sx={{ m: 1, width: '25ch' }}
+                        >
+                            {charRaces.map((race) => (
+                                <MenuItem key={race.label} value={race.label}>
+                                    {race.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField 
+                            required
+                            id='outlined-select-required'
+                            select
+                            label='Gender'
+                            defaultValue='Select'
+                            sx={{ m: 1, width: '25ch' }}
+                        >
+                            {charGender.map((gender) => (
+                                <MenuItem key={gender.label} value={gender.label}>
+                                    {gender.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField 
+                            required
+                            id='outlined-select-required'
+                            select
+                            label='Class'
+                            defaultValue='Select'
+                            sx={{ m: 1, width: '25ch' }}
+                        >
+                            {charClasses.map((charClass) => (
+                                <MenuItem key={charClass.label} value={charClass.label}>
+                                    {charClass.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    <TextField
                         required
-                        id='outlined-required'
-                        label='Character Name'
+                        fullWidth
+                        id='outlined-multiline-static'
+                        label='Background'
+                        multiline
+                        rows={5}
+                        defaultValue='Write your background story.'
+                        sx={{ m: 1 }}
                     />
-                    <TextField 
-                        required
-                        id='outlined-select-required'
-                        select
-                        label='Level'
-                        defaultValue='1'
-                        helperText='Select Starting Level'
-                    >
-                        {charLevels.map((level) => (
-                            <MenuItem key={level.value} value={level.value}>
-                                {level.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField 
-                        required
-                        id='outlined-select-required'
-                        select
-                        label='Race'
-                        defaultValue='Select'
-                    >
-                         {charRaces.map((race) => (
-                            <MenuItem key={race.label} value={race.label}>
-                                {race.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField 
-                        required
-                        id='outlined-select-required'
-                        select
-                        label='Gender'
-                        defaultValue='Select'
-                    >
-                         {charGender.map((gender) => (
-                            <MenuItem key={gender.label} value={gender.label}>
-                                {gender.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField 
-                        required
-                        id='outlined-select-required'
-                        select
-                        label='Class'
-                        defaultValue='Select'
-                    >
-                         {charClasses.map((charClass) => (
-                            <MenuItem key={charClass.label} value={charClass.label}>
-                                {charClass.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField />
                     <TextField />
                     <TextField />
                     <TextField />
