beae4373a4839f06fff7bd0e3b37e7b5349aba4b
Phil Simpson <phil_simpson@att.net>
2023-03-29 19:58:50 -0500

    Character test array

---
+++ src/app/shared/CHARACTERS.js
@@ -0,0 +1,22 @@
+export const CHARACTERS = [
+    {
+        id: 0,
+        name: 'Davon',
+        level: 1,
+        gender: 'male',
+        race: 'halfling',
+        class: 'rogue',
+        backstory: 'A feisty and crafty halfing, Davon Lightfoot favors daggers and a rapier, and sometimes a hand crossbow, during his rogueish piracy adventures.',
+        player: 'Phil Simpson',
+    },
+    {
+        id: 1,
+        name: 'Teirora',
+        level: 1,
+        gender: 'male',
+        race: 'elf',
+        class: 'ranger',
+        backstory: 'Teirora comes from the enchanting mountains and forests high above the flood plains, where there is warmth in the summer and extreme cold during winter.  The forests have always captured her heart and keep her from venturing to far from home.',
+        player: 'Andrea Simpson',
+    }
+]
